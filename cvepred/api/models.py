e10de8a4bc862f6878a8cf690896bdb482e22752
Łukasz Gęborys <s25911@pjwstk.edu.pl>
2025-01-13 19:27:26 +0100

    feat: add scalar for docgen

--- cvepred/api/models.py
+++ cvepred/api/models.py
@@ -1,28 +1,36 @@
 from pydantic import BaseModel
 import typing as ty
 
-# attackVector             category
-# attackComplexity         category
-# privilegesRequired       category
-# userInteraction          category
-# scope                    category
-# confidentialityImpact    category
-# integrityImpact          category
-# availabilityImpact       category
-# hasExploit                   bool
-
 
 class CveModel(BaseModel):
-    attackVector: str
-    attackComplexity: str
-    privilegesRequired: str
-    userInteraction: str
-    scope: str
-    confidentialityImpact: str
-    integrityImpact: str
-    availabilityImpact: str
+    attackVector: ty.Literal["NETWORK", "ADJACENT_NETWORK", "LOCAL", "PHYSICAL"]
+    attackComplexity: ty.Literal["LOW", "HIGH"]
+    privilegesRequired: ty.Literal["NONE", "LOW", "HIGH"]
+    userInteraction: ty.Literal["NONE", "REQUIRED"]
+    scope: ty.Literal["UNCHANGED", "CHANGED"]
+    confidentialityImpact: ty.Literal["NONE", "LOW", "HIGH"]
+    integrityImpact: ty.Literal["NONE", "LOW", "HIGH"]
+    availabilityImpact: ty.Literal["NONE", "LOW", "HIGH"]
     # hasExploit: bool
 
 
 class CveModels(BaseModel):
     data: ty.List[CveModel]
+
+
+class CveNvdIdsModel(BaseModel):
+    ids: ty.List[str]
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "ids": [
+                        "CVE-2016-2183",
+                        "CVE-2021-3438",
+                        # "CVE-2016-2137",
+                    ]
+                }
+            ]
+        }
+    }
